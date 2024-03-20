3ecb422a406cd60fa39e462d2a697bcbe05b2b4b
Julian Kennedy <juliankenn@gmail.com>
2024-03-20 23:15:13 +0200

    added executor to run mapserv

--- src/main/java/io/spatializer/core/controllers/HomeController.java
+++ src/main/java/io/spatializer/core/controllers/HomeController.java
@@ -1,8 +1,17 @@
 package io.spatializer.core.controllers;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 @Controller
@@ -11,7 +20,21 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     String getIndex() {
-        return "Hello World 4";
+        var exe = "/usr/bin/mapserv";
+        var commandLine = CommandLine.parse(exe);
+        var executor = DefaultExecutor.builder().get();
+
+        var baos = new ByteArrayOutputStream();
+        var streamHandler = new PumpStreamHandler(baos);
+        executor.setStreamHandler(streamHandler);
+
+        try {
+            executor.execute(commandLine);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        var stdout = baos.toString();
+        return stdout;
     }
 
 }
